15d4447109184302dccc4a9b4b770b89cac5515b
Pontus Stenetorp <pontus stenetorp se@localhost>
2010-11-09 21:18:40 +0900

    Minor clean-up.

--- src/org/u_compare/gui/WorkflowSplitPane.java
+++ src/org/u_compare/gui/WorkflowSplitPane.java
@@ -1,10 +1,7 @@
 package org.u_compare.gui;
 
-import java.awt.Dimension;
-
 import javax.swing.JSplitPane;
 import javax.swing.JTabbedPane;
-import javax.swing.SwingUtilities;
 
 import org.u_compare.gui.model.Component;
 import org.u_compare.gui.model.DescriptionChangeListener;
@@ -14,16 +11,19 @@ import org.u_compare.gui.model.SavedStatusChangeListener;
  * Holds the workflow pane and the console pane.
  * 
  * @author pontus
+ * @author luke
  * @version 2009-08-28
  */
 @SuppressWarnings("serial")
-public class WorkflowSplitPane extends JSplitPane implements DescriptionChangeListener, SavedStatusChangeListener{
+public class WorkflowSplitPane extends JSplitPane
+	implements DescriptionChangeListener, SavedStatusChangeListener {
+	
 	private ConsolePane consolePane;
 	private WorkflowPane workflowPane;
 	
 	// Configuration
 	private static final boolean ONE_TOUCH_EXPANDABLE = true;
-	private static final double SEPARATOR_START_LOCATION_FROM_BOTTOM = 0.2;
+	private static final double SEPARATOR_START_LOCATION_FROM_BOTTOM = 0.2D;
 	private static final int SPLIT_ORIENTATION = JSplitPane.VERTICAL_SPLIT;
 	
 	private IconizedCloseableTabFlapComponent tab;
@@ -59,22 +59,25 @@ public class WorkflowSplitPane extends JSplitPane implements DescriptionChangeLi
 	
 	@Override
 	public void ComponentDescriptionChanged(Component component) {
-		if(tab!=null){
-			tabPane.setTitleAt(tabPane.indexOfComponent(this), WorkflowTabbedPane.cleanTitle(component.getName()) + (component.checkUnsavedChanges()?"*":""));
+		if(tab != null){
+			tabPane.setTitleAt(tabPane.indexOfComponent(this),
+					WorkflowTabbedPane.cleanTitle(component.getName())
+					+ (component.checkUnsavedChanges() ? "*" : ""));
 		}
 	}
 
 	@Override
 	public void savedStatusChanged(Component component) {
-		
 		System.out.println("Saved change listner notified");
-		if(tab!=null){
-			if(component.checkUnsavedChanges()){
-				tabPane.setTitleAt(tabPane.indexOfComponent(this), WorkflowTabbedPane.cleanTitle(component.getName())+"*");
-			}else{
-				tabPane.setTitleAt(tabPane.indexOfComponent(this), WorkflowTabbedPane.cleanTitle(component.getName()));
+		if (tab != null) {
+			if(component.checkUnsavedChanges()) {
+				tabPane.setTitleAt(tabPane.indexOfComponent(this),
+						WorkflowTabbedPane.cleanTitle(component.getName())
+						+ "*");
+			} else {
+				tabPane.setTitleAt(tabPane.indexOfComponent(this),
+						WorkflowTabbedPane.cleanTitle(component.getName()));
 			}
 		}
-		
 	}
 }

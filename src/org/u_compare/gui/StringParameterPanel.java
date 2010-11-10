7859f0ff16d3ef7f9cfccf91225b1488b4b8694b
Pontus Stenetorp <pontus stenetorp se@localhost>
2010-11-10 20:51:41 +0900

    Clean-up squad, some removal of old code and standard conformity.

---
+++ src/org/u_compare/gui/StringParameterPanel.java
@@ -0,0 +1,44 @@
+package org.u_compare.gui;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import org.u_compare.gui.control.ActionFocusListener;
+import org.u_compare.gui.model.parameters.Parameter;
+
+@SuppressWarnings("serial")
+public class StringParameterPanel extends ParameterPanel {
+
+	ActionFocusListener controller;
+	
+	JTextField textField;
+	
+	public StringParameterPanel(Parameter param, ActionFocusListener control,
+			String initialValue){
+		
+		this.controller = control;
+		
+		this.add(new JLabel(param.getDescription()));
+		
+		textField = new JTextField(initialValue);
+		textField.addActionListener(controller);
+		textField.addFocusListener(controller);
+		
+		this.add(textField);
+	}
+	
+	public String getString(){
+		return textField.getText();
+	}
+	
+	public void setString(String value){
+		textField.setText(value);
+	}
+
+	@Override
+	public void setLockedStatus(boolean locked) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ src/org/u_compare/gui/control/DropTargetController.java
@@ -0,0 +1,85 @@
+package org.u_compare.gui.control;
+
+import org.u_compare.gui.DropTargetJPanel;
+
+public class DropTargetController implements DragAndDropComponentController {
+
+	private ComponentController parent;
+	public DropTargetJPanel view;
+	
+	/**
+	 * Must also call the set view method.
+	 * 
+	 * @param parent
+	 */
+	public DropTargetController(ComponentController parent){
+		this.parent = parent;
+	}
+	
+	public void setView(DropTargetJPanel view) {
+		this.view = view;
+	}
+	
+	public DropTargetController(ComponentController parent, DropTargetJPanel view){
+		this.parent = parent;
+		this.view = view;
+	}
+	
+	/**
+	 * This drop target is no longer necessary in the view so is deleted.
+	 */
+	public void dropTargetRemoved(){
+		parent.dropTargetRemoved(this);
+	}
+
+	@Override
+	public void setDragged() {
+	
+		// TODO throw an error	
+	}
+
+	@Override
+	public void somethingDroppedOnComponent() {
+		
+		parent.somethingDroppedOnChild(this);
+		
+	}
+
+	@Override
+	public void setDragEnter() {
+
+		if(parent.droppableOnChild(this)){
+			view.setDragOverHighlightingDroppable();
+		}else{
+			view.setDragOverHighlightingUndroppable();
+		}
+		
+	}
+
+	@Override
+	public void setDragExit() {
+
+		view.clearDragOverHighlighting();
+		
+	}
+
+	public void setIntermediate() {
+		
+		view.setIntermediate();
+		
+	}
+
+	public void setSolitaryDropTarget() {
+		
+		view.setSolitaryDropTarget();
+		
+	}
+	
+	public void clearSolitaryDropTarget(){
+
+		view.clearSolitaryDropTarget();
+		
+	}
+
+	
+}

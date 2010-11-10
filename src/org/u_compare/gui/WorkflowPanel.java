3fa41226ceda65f908f49cd9d7ae3d8721ce9756
luke.mccrohon <luke.mccrohon@localhost>
2010-11-10 21:19:47 +0900

    Added workflow tab icons from http://openiconlibrary.sourceforge.net/

---
+++ src/org/u_compare/gui/WorkflowPanel.java
@@ -0,0 +1,25 @@
+package org.u_compare.gui;
+
+import org.u_compare.gui.control.ComponentController;
+import org.u_compare.gui.model.AggregateComponent;
+import org.u_compare.gui.model.Component;
+import org.u_compare.gui.model.Workflow;
+
+@SuppressWarnings("serial")
+public class WorkflowPanel extends ComponentPanel {
+
+	public WorkflowPanel(Workflow component,
+			ComponentController controller){
+		super(controller);
+		
+		initialConfiguration(component, controller);
+		
+		component.registerSubComponentsChangedListener(this);
+		
+		
+		
+		
+		
+	}
+	
+}

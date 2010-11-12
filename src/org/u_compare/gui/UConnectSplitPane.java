e164f3d3e9462d5079b69d1335086cbded5cfeda
luke.mccrohon <luke.mccrohon@localhost>
2010-11-12 22:11:22 +0900

    Extracted workflowPane construction out of the GUITestingHarness into WorkflowPaneController

--- src/org/u_compare/gui/UConnectSplitPane.java
+++ src/org/u_compare/gui/UConnectSplitPane.java
@@ -1,5 +1,7 @@
 package org.u_compare.gui;
 
+import javax.swing.JComponent;
+import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 import javax.swing.border.Border;
 import javax.swing.plaf.basic.BasicSplitPaneDivider;
@@ -10,16 +12,16 @@ import org.u_compare.gui.library.LibraryPane;
 @SuppressWarnings("serial")
 public class UConnectSplitPane extends JSplitPane {
 
-	private WorkflowTabbedPane workflowPane;
-	private LibraryPane libraryPane;
+	private JComponent workflowPane;
+	private JComponent libraryPane;
 	
 	// Configuration
 	private static final boolean ONE_TOUCH_EXPANDABLE = true;
 	private static final int SEPARATOR_START_LOCATION_FROM_LEFT = 1280 / 2;
 	private static final int SPLIT_ORIENTATION = JSplitPane.HORIZONTAL_SPLIT;
 	
-	public UConnectSplitPane(WorkflowTabbedPane workflowPane,
-			LibraryPane libraryPane) {
+	public UConnectSplitPane(JComponent workflowPane,
+			JComponent libraryPane) {
 		
 		this.workflowPane = workflowPane;
 		this.libraryPane = libraryPane;

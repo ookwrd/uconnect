464c72b3c7b58d175291c7c632ea163369c55303
olaf <olaf@localhost>
2010-11-13 17:48:39 +0900

    button icons for workflow start/stop

--- src/org/u_compare/gui/component/WorkflowControlPanel.java
+++ src/org/u_compare/gui/component/WorkflowControlPanel.java
@@ -1,9 +1,12 @@
 package org.u_compare.gui.component;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URL;
 
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -22,28 +25,37 @@ import org.u_compare.gui.model.Workflow.WorkflowStatus;
  */
 
 @SuppressWarnings("serial")
-public class WorkflowControlPanel extends JPanel
-	implements WorkflowStatusListener {
+public class WorkflowControlPanel extends JPanel implements
+		WorkflowStatusListener {
 
 	public final static String STATUS_PREFIX = "Status: ";
-	
+	private static final int BUTTON_DECREMENT = 0;
+	public static final String ICON_RUN_PATH = "../gfx/icon_start.png";
+	public static final String ICON_STOP_PATH = "../gfx/icon_stop.png";
+
+	private static boolean iconsLoaded = false;
+
+	private static ImageIcon runIcon;
+	private static ImageIcon stopIcon;
+
 	private Workflow component;
 	private WorkflowController controller;
-	
+
 	private ActionListener playListener;
 	private ActionListener stopListener;
-	
+
 	private JLabel statusLabel;
+
 	private JButton runButton;
 	private JButton stopButton;
-	
+
 	public WorkflowControlPanel(Workflow component,
 			WorkflowController controller) {
 		super();
-		
+
 		this.component = component;
 		this.controller = controller;
-		
+
 		playListener = new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				playWorkflow();
@@ -55,34 +67,73 @@ public class WorkflowControlPanel extends JPanel
 				stopWorkflow();
 			}
 		};
-		
+
 		setOpaque(false);
-		setLayout(new BoxLayout(this,BoxLayout.X_AXIS));
-		
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+
 		statusLabel = new JLabel(STATUS_PREFIX
-				+ ((Workflow)component).getStatus());
+				+ ((Workflow) component).getStatus());
 		this.add(statusLabel);
-			
-		runButton = new JButton("start");
+
+		this.loadIcons();
+
+		Dimension buttonSize;
+		
+		//run button
+		System.out.println("button");
+		runButton = new JButton(runIcon);
+		buttonSize = new Dimension(runIcon.getIconWidth() - BUTTON_DECREMENT,
+				runIcon.getIconHeight() - BUTTON_DECREMENT);
+		System.out.println("button");
+		//stopButton.setPreferredSize(buttonSize);
+		System.out.println("butn");
+		runButton.setFocusPainted(false); // This may be needed for a mac
+											// specific behaviou
+		System.out.println("tton");
 		runButton.addActionListener(playListener);
 		this.add(runButton);
-			
-		stopButton = new JButton("stop");
+
+		//stop button
+		stopButton = new JButton(stopIcon);
+		buttonSize = new Dimension(stopIcon.getIconWidth() - BUTTON_DECREMENT,
+				stopIcon.getIconHeight() - BUTTON_DECREMENT);
+		//stopButton.setPreferredSize(buttonSize);
+		stopButton.setFocusPainted(false);
 		stopButton.addActionListener(stopListener);
-		add(stopButton);
-		
+		this.add(stopButton);
+		System.out.println("stop button ok");
+
 		component.registerWorkflowStatusListener(this);
-		
+
 		setBorder(new EtchedBorder());
 	}
-	
+
+	protected static synchronized void loadIcons() {
+		if (WorkflowControlPanel.iconsLoaded == true) {
+			return;
+		}
+
+		URL image_url;
+		image_url = ComponentPanel.class
+				.getResource(WorkflowControlPanel.ICON_RUN_PATH);
+		assert image_url != null;
+		WorkflowControlPanel.runIcon = new ImageIcon(image_url, "Run");
+
+		image_url = ComponentPanel.class
+				.getResource(WorkflowControlPanel.ICON_STOP_PATH);
+		assert image_url != null;
+		WorkflowControlPanel.stopIcon = new ImageIcon(image_url, "Stop");
+
+		WorkflowControlPanel.iconsLoaded = true;
+		return;
+	}
+
 	private void stopWorkflow() {
-		WorkflowStatus currentStatus = ((Workflow)component).getStatus();
-		if(currentStatus == WorkflowStatus.READY
+		WorkflowStatus currentStatus = ((Workflow) component).getStatus();
+		if (currentStatus == WorkflowStatus.READY
 				|| currentStatus == WorkflowStatus.PAUSED) {
 			controller.workflowPlayRequest();
-		}
-		else if (currentStatus == WorkflowStatus.RUNNING
+		} else if (currentStatus == WorkflowStatus.RUNNING
 				|| currentStatus == WorkflowStatus.INITIALIZING) {
 			controller.workflowPauseRequest();
 		}
@@ -91,11 +142,11 @@ public class WorkflowControlPanel extends JPanel
 	private void playWorkflow() {
 		controller.workflowStopRequest();
 	}
-	
+
 	@Override
 	public void workflowStatusChanged(Workflow workflow) {
 		statusLabel.setText(STATUS_PREFIX + workflow.getStatus());
-		//TODO: Update Buttons
+		// TODO: Update Buttons
 	}
-	
+
 }

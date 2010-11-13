5d4e28e8d238e749071acc1734a842f899355c6a
luke.mccrohon <luke.mccrohon@localhost>
2010-11-13 19:05:56 +0900

    Deselection of TypeListPanel items on loss of focus.

--- src/org/u_compare/gui/component/TypeListPanel.java
+++ src/org/u_compare/gui/component/TypeListPanel.java
@@ -1,16 +1,22 @@
 package org.u_compare.gui.component;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 public class TypeListPanel extends JPanel {
 
 	private JPanel buttons;
+	private JList list;
 	
 	public TypeListPanel(String[] options){
 		
@@ -20,9 +26,35 @@ public class TypeListPanel extends JPanel {
 		setLayout(new BoxLayout(this,
 				BoxLayout.Y_AXIS));
 		
-		JList list = new JList(options);
+		
+		
+
+		ListSelectionListener listSelectionListener = new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent evt) {
+				if (evt.getValueIsAdjusting())
+					return;
+				System.out.println("Selected from " + evt.getFirstIndex() + " to " + evt.getLastIndex());
+			}
+		};
+		
+		FocusListener listFocusListener = new FocusListener() {
+			
+			public void focusGained(FocusEvent e) {
+				//buttons.setVisible(true);
+			}
+
+			public void focusLost(FocusEvent e) {
+				buttons.setVisible(false);
+				list.clearSelection();//TODO make it so the buttons dont clear this??
+			}
+		};
+		
+		list = new JList(options);
 		list.setFixedCellWidth(150);
 		list.setBackground(getBackground());
+		
+		list.addListSelectionListener(listSelectionListener);
+		list.addFocusListener(listFocusListener);
 		
 		this.add(list);
 		

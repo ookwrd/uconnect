bc035238b5b71089b42f79cd0d08d3c09e4d6f8b
olaf <olaf@localhost>
2010-11-17 23:06:56 +0900

    refactored TopPanel

---
+++ src/org/u_compare/gui/component/TypeListPanel.java
@@ -0,0 +1,167 @@
+package org.u_compare.gui.component;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JButton;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import org.u_compare.gui.model.AnnotationType;
+import org.u_compare.gui.model.InputOutputChangeListener;
+import org.u_compare.gui.model.LockedStatusChangeListener;
+
+@SuppressWarnings("serial")
+public class TypeListPanel extends JPanel implements LockedStatusChangeListener, InputOutputChangeListener {
+
+	public static final int INPUTS_LIST = 0;
+	public static final int OUTPUTS_LIST = 1;
+	
+	private JPanel buttons;
+	private JButton deleteButton;
+	private JButton addButton;
+	private JList list;
+	
+	private org.u_compare.gui.model.Component component;
+	
+	private int listType;
+	
+	public TypeListPanel(org.u_compare.gui.model.Component component, String[] options){
+		
+		this.component = component;
+		
+
+		
+		//Options extracted more directly from model
+		
+		setOpaque(false);
+		setLayout(new BoxLayout(this,
+				BoxLayout.Y_AXIS));
+		
+		
+		
+
+		ListSelectionListener listSelectionListener = new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent evt) {
+				if (evt.getValueIsAdjusting())
+					return;
+				System.out.println("Selected from " + evt.getFirstIndex() + " to " + evt.getLastIndex());
+			}
+		};
+		
+		FocusListener listFocusListener = new FocusListener() {
+			
+			public void focusGained(FocusEvent e) {
+				if(!TypeListPanel.this.component.getLockedStatus()){
+					buttons.setVisible(true);
+				}
+			}
+
+			public void focusLost(FocusEvent e) {
+				
+				Object source = e.getOppositeComponent();
+				
+				if(source==null
+						|| source.equals(list) 
+						|| source.equals(addButton) 
+						|| source.equals(deleteButton)){
+					return;
+				}
+				
+				list.clearSelection();
+				buttons.setVisible(false);
+			}
+		};
+		
+		list = new JList(options);
+		list.setFixedCellWidth(150);
+		list.setBackground(getBackground());
+		
+		list.addListSelectionListener(listSelectionListener);
+		list.addFocusListener(listFocusListener);
+		
+		this.add(list);
+		
+		
+		buttons = new JPanel();
+		buttons.setLayout(new FlowLayout(FlowLayout.TRAILING));
+		
+		deleteButton = new JButton("Delete Type");
+		addButton = new JButton("Add Type");
+		
+		buttons.add(deleteButton);
+		buttons.add(addButton);
+		
+		buttons.setVisible(false);
+		
+		this.add(buttons);
+		
+		configureLockStatus();
+
+		component.registerLockedStatusChangeListener(this);
+		component.registerInputOutputChangeListener(this);
+		
+		//TODO deletion
+	}
+	
+	private void configureLockStatus(){
+		if(component.getLockedStatus()){
+			//Sets the list items unselect-able, without changing their appearance
+			list.setEnabled(false);
+		    list.setCellRenderer(new DefaultListCellRenderer() {
+		        public Component getListCellRendererComponent(
+		            JList list,
+		            Object value,
+		            int index,
+		            boolean isSelected,
+		            boolean cellHasFocus) {
+
+		            super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+		            this.setEnabled(true);
+		            return this;
+		        }
+		    });
+		}else{
+			list.setEnabled(true);
+		}
+	}
+
+	private void rebuildListContents(){
+		assert(list!=null);
+		list.removeAll();
+		
+		switch(listType){
+		case INPUTS_LIST:
+			for(AnnotationType annotation : component.getInputTypes()){
+				//TODO
+			}
+			break;
+		case OUTPUTS_LIST:
+			for(AnnotationType annotation : component.getOutputTypes()){
+				//TODO
+			}
+			break;
+		default:
+			throw new Error("TypeListPanel listType not set to a valid value: " + listType);
+		}
+	}
+	
+	@Override
+	public void lockStatusChanged(org.u_compare.gui.model.Component component) {
+		configureLockStatus();
+	}
+
+	@Override
+	public void inputOutputChanged(org.u_compare.gui.model.Component component) {
+		
+	}
+	
+}
---
+++ src/org/u_compare/gui/control/WorkflowController.java
@@ -0,0 +1,72 @@
+package org.u_compare.gui.control;
+
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
+import org.u_compare.gui.WorkflowPanel;
+import org.u_compare.gui.component.ComponentPanel;
+import org.u_compare.gui.model.Component;
+import org.u_compare.gui.model.InvalidStatusException;
+import org.u_compare.gui.model.Workflow;
+
+public class WorkflowController extends ComponentController {
+
+	
+	public WorkflowController(Workflow component, boolean showWorkflowControlPanel, boolean showWorkflowDetails, boolean allowEditing) {
+		super(allowEditing);
+		
+		this.component = component;
+		this.componentView = new WorkflowPanel(component, this, showWorkflowControlPanel, showWorkflowDetails);
+		
+		
+	}
+	
+	@Override
+	public WorkflowPanel getView(){
+		
+		return (WorkflowPanel)this.componentView;
+		
+	}
+	
+	
+	public void workflowPlayRequest(){
+
+		assert(component.isWorkflow());
+	
+		try {
+			((Workflow)component).runWorkflow();
+		} catch (InvalidStatusException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	public void workflowPauseRequest(){
+		
+		assert(component.isWorkflow());
+		
+		try {
+			((Workflow)component).runWorkflow();
+		} catch (InvalidStatusException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void workflowStopRequest(){
+		
+		assert(component.isWorkflow());
+		
+		try {
+			((Workflow)component).runWorkflow();
+		} catch (InvalidStatusException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+}

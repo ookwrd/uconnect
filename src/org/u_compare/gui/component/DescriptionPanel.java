e7baf3dd7a1045bcdc710521702b09ef7998f1d6
olaf <olaf@localhost>
2010-11-24 05:35:55 +0900

    added a button to exit the description editing mode

--- src/org/u_compare/gui/component/DescriptionPanel.java
+++ src/org/u_compare/gui/component/DescriptionPanel.java
@@ -3,6 +3,8 @@ package org.u_compare.gui.component;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,6 +13,7 @@ import java.awt.event.FocusListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -21,39 +24,44 @@ import org.u_compare.gui.model.Component;
 import org.u_compare.gui.model.DescriptionChangeListener;
 
 @SuppressWarnings("serial")
-public class DescriptionPanel extends JPanel implements DescriptionChangeListener{
+public class DescriptionPanel extends JPanel implements
+		DescriptionChangeListener {
 
 	private static final int DESCRIPTION_PANEL_PADDING = 5;
-	
+
 	public final Color defaultColor = getBackground();
-	
+
 	private final ComponentController controller;
 	private final Component component;
 
 	private ActionListener descriptionListener;
 	private FocusListener descriptionFocusListener;
-	
+
 	private JTextArea description;
 	private JTextArea editableDescription;
 	private String descriptionText;
-	
-	public DescriptionPanel(ComponentController controller, Component component){
+	private JButton endEditingButton;
+
+	private ActionListener endEditingListener;
+
+	public DescriptionPanel(ComponentController controller, Component component) {
 		super();
-	
+
 		this.controller = controller;
 		this.component = component;
-		
+
 		descriptionListener = new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
 				setDescription(editableDescription.getText());
 				editableDescription.setVisible(false);
 				description.setVisible(true);
+				endEditingButton.setVisible(true);
 			}
 		};
-		
+
 		descriptionFocusListener = new FocusListener() {
-			
+
 			public void focusGained(FocusEvent e) {
 			}
 
@@ -61,84 +69,108 @@ public class DescriptionPanel extends JPanel implements DescriptionChangeListene
 				setDescription(editableDescription.getText());
 				editableDescription.setVisible(false);
 				description.setVisible(true);
+				endEditingButton.setVisible(false);
 			}
 		};
-		
-		// add a description panel under the top panel
-		CardLayout descriptionLayout = new CardLayout();
+
+		endEditingListener = new ActionListener() {
+
+			public void actionPerformed(ActionEvent e) {
+				//TODO remove the useless lines
+				System.out.println("PUSHED STOP EDITING");
+				setDescription(editableDescription.getText());
+				editableDescription.setVisible(false);
+				description.setVisible(true);
+				endEditingButton.setVisible(false);
+			}
+		};
+
+		// add a description panel under the top panel, and first set the layout 
+		BorderLayout descriptionLayout = new BorderLayout();
 		setLayout(descriptionLayout);
-		
+
 		setOpaque(false);
-		setBorder(new EmptyBorder(
-				new Insets(DESCRIPTION_PANEL_PADDING, DESCRIPTION_PANEL_PADDING,
-						DESCRIPTION_PANEL_PADDING, DESCRIPTION_PANEL_PADDING)));
-		
+		setBorder(new EmptyBorder(new Insets(DESCRIPTION_PANEL_PADDING,
+				DESCRIPTION_PANEL_PADDING, DESCRIPTION_PANEL_PADDING,
+				DESCRIPTION_PANEL_PADDING)));
+
 		descriptionText = component.getDescription();
-		
+
 		description = new JTextArea(descriptionText);
 		description.setBackground(defaultColor);
 		description.setLineWrap(true);
 		description.setWrapStyleWord(true);
 		description.setEditable(false);
-		
+
 		editableDescription = new JTextArea(descriptionText);
 		editableDescription.setBackground(Color.WHITE);
 		editableDescription.setLineWrap(true);
 		editableDescription.setWrapStyleWord(true);
 		editableDescription.setEditable(true);
 		editableDescription.setVisible(false);
+
+		endEditingButton = new JButton("Save");
+		endEditingButton.setActionCommand("End description editing");
+		endEditingButton.addActionListener(endEditingListener);
+		endEditingButton.setVisible(false);
 		
-		add(description, BorderLayout.LINE_START);
+		add(description, BorderLayout.PAGE_START);
 		add(editableDescription, BorderLayout.LINE_START);
+		add(endEditingButton, BorderLayout.LINE_END);
+		//TODO change the layout to cardlayout if possible
+		
 		setBackground(Color.WHITE);
 		
 		description.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent e) {
 				if (e.getClickCount() == 2) {
-					//JPanel target = (JPanel) e.getSource();
-					if(!DescriptionPanel.this.component.getLockedStatus()){
-						//setDescription(description.getText());
+					// JPanel target = (JPanel) e.getSource();
+					if (!DescriptionPanel.this.component.getLockedStatus()) {
+						// setDescription(description.getText());
 						description.setVisible(false);
 						editableDescription.setVisible(true);
-						
-						//editableDescription.requestFocusInWindow();
+
+						// editableDescription.requestFocusInWindow();
 						editableDescription.setVisible(false);
 						editableDescription.setVisible(true);
 						editableDescription.requestFocus();
+
+						endEditingButton.setVisible(true);
 					}
 				}
 			}
 		});
 
-		//editableDescription.addActionListener(descriptionListener); // useless: not a text field anymore
+		// editableDescription.addActionListener(descriptionListener); //
+		// useless: not a text field anymore
 		editableDescription.addFocusListener(descriptionFocusListener);
-		
-		//Register Listeners
+
+		// Register Listeners
 		component.registerComponentDescriptionChangeListener(this);
-		
+
 	}
-	
-	//TODO this might be set directly by the controller
+
+	// TODO this might be set directly by the controller
 	protected void setDescription(String descriptionText) {
 		descriptionText = descriptionText.trim();
-		/*//description right trim
-		String tmp = "";
-		for(int i=descriptionText.length()-1; i>=0; i++) {
-			if (descriptionText.charAt(i)==a) tmp+="descriptionText.charAt(i);
-		}
-		descriptionText = tmp;*/ 
+		/*
+		 * //description right trim String tmp = ""; for(int
+		 * i=descriptionText.length()-1; i>=0; i++) { if
+		 * (descriptionText.charAt(i)==a) tmp+="descriptionText.charAt(i); }
+		 * descriptionText = tmp;
+		 */
 		this.descriptionText = descriptionText;
 		description.setText(descriptionText);
 		editableDescription.setText(descriptionText);
 		this.controller.setDescription(descriptionText);
 	}
-	
+
 	@Override
 	public void ComponentDescriptionChanged(Component component1) {
-		
+
 		System.out.println("Components name changed to: "
 				+ component.getTitle());
-		//TODO
+		// TODO
 	}
-	
+
 }

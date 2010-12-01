318a34f9ef7ddc4279a6fd0345a593ed40f96520
luke.mccrohon <luke.mccrohon@localhost>
2010-12-02 02:51:58 +0900

    Refactored parameter factory into AbstractParameter Helper method for creating component from UIMA descriptor now set vendor, version and copyright information

--- src/org/u_compare/gui/model/PrimitiveUIMAComponent.java
+++ src/org/u_compare/gui/model/PrimitiveUIMAComponent.java
@@ -21,6 +21,7 @@ import org.apache.uima.tools.components.InlineXmlCasConsumer;
 import org.apache.uima.util.InvalidXMLException;
 import org.apache.uima.util.XMLInputSource;
 import org.omg.CORBA.PUBLIC_MEMBER;
+import org.u_compare.gui.model.parameters.AbstractParameter;
 import org.u_compare.gui.model.parameters.BooleanParameter;
 import org.u_compare.gui.model.parameters.IntegerParameter;
 import org.u_compare.gui.model.parameters.Parameter;
@@ -188,69 +189,26 @@ public class PrimitiveUIMAComponent extends AbstractComponent {
 	
 	protected void processResourceMetaData(ResourceMetaData metaData){
 		
+		//Basic metaData
 		setTitle(metaData.getName());
 		setDescription(metaData.getDescription());
-		//TODO vendor
-		//TODO version
-		//TODO copyright
+		setVendor(metaData.getVendor());
+		setVersion(metaData.getVersion());
+		setCopyright(metaData.getCopyright());
 		
+		//Parameters
 		ArrayList<Parameter> parameters = new ArrayList<Parameter>(); 
-		
 		ConfigurationParameterSettings settings = metaData.getConfigurationParameterSettings();
 		for(ConfigurationParameter param : metaData.getConfigurationParameterDeclarations().getConfigurationParameters()){
 
-			Parameter newParameter = constructParameter(param, settings.getParameterValue(param.getName()));
+			Parameter newParameter = AbstractParameter.constructParameter(param, settings.getParameterValue(param.getName()));
 			parameters.add(newParameter);
 			
 		}
-		
 		//TODO make the parameter constructors work first
 		setConfigurationParameters(parameters);
 		
 	}
-	
-	//TODO move to parameter interface as static factory
-	protected Parameter constructParameter(ConfigurationParameter param, Object value){
-		
-		Parameter retVal;
-		
-		String name = param.getName();
-		String description = param.getDescription();
-		boolean multivalued = param.isMultiValued();
-		boolean mandatory = param.isMandatory();
-		
-		String type = param.getType();//Why are the types Strings? I cant do a switch statement...
-		if(type.equals(ConfigurationParameter.TYPE_BOOLEAN)) {
-			if(multivalued){
-				retVal = null; //TODO
-			}else{
-				retVal = new BooleanParameter(name, description, mandatory, value!=null?(Boolean)value:null);
-			}
-		} else if (type.equals(ConfigurationParameter.TYPE_FLOAT)) {
-			if(multivalued){
-				retVal = null; //TODO
-			}else{
-				retVal = null; //TODO
-			}
-		} else if (type.equals(ConfigurationParameter.TYPE_INTEGER)) {
-			if(multivalued){
-				retVal = null; //TODO
-			}else{
-				retVal = new IntegerParameter(name, description, mandatory, value!=null?(Integer)value:null);
-			}
-		} else if (type.equals(ConfigurationParameter.TYPE_STRING)) {
-			if(multivalued){
-				retVal = null; //TODO
-			}else{
-				retVal = new StringParameter(name, description, mandatory, value!=null?(String)value:null);
-			}
-		} else {
-			retVal = null; //TODO throw an error here. 
-		}
-		
-		return retVal;
-	}
-	
 	
 	public static Component constructUIMAComponent(){
 		//TODO need as there are multiple kinds

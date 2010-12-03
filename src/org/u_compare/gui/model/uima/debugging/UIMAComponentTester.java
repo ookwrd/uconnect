906cc8345a98a915e7ed93b06b17fe2420db50c2
luke.mccrohon <luke.mccrohon@localhost>
2010-12-04 02:07:36 +0900

    Added unittest for parsing/deparsing XML Discriptors with multivalued parameters.

--- src/org/u_compare/gui/model/uima/debugging/UIMAComponentTester.java
+++ src/org/u_compare/gui/model/uima/debugging/UIMAComponentTester.java
@@ -46,6 +46,11 @@ public class UIMAComponentTester {
 		inOutTest("src/org/u_compare/gui/model/uima/debugging/BasicAEwithSingleValuedParametersAndValues.xml");
 	}
 	
+	@Test
+	public void inOutTestBasicMultivaluedParameters() throws InvalidXMLException, IOException {
+		inOutTest("src/org/u_compare/gui/model/uima/debugging/BasicAEwithMultivaluedParameters.xml");
+	}
+	
 	/**
 	 * Performs a test to ensure the XML produced from loading a XMLDescriptor into the model and then
 	 * extracting it again matches the original XML.

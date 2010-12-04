9377e6215cec9848cfa3f1a748eae5458d5c6438
Pontus Stenetorp <pontus stenetorp se@localhost>
2010-12-04 16:56:19 +0900

    The TestWindow now centers correctly on all devices including multi-monitor devices such as when we demo. This code should be extracted to a more general class in time and certainly be incorporated into our window class once we get one.

--- src/org/u_compare/gui/debugging/TestWindow.java
+++ src/org/u_compare/gui/debugging/TestWindow.java
@@ -2,8 +2,13 @@ package org.u_compare.gui.debugging;
 
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 /**
  * Used as a shorthand to test GUI components in small steps.
@@ -22,6 +27,8 @@ public class TestWindow extends JFrame {
 	 */
 	private static final Dimension MINIMUM_SIZE = new Dimension(640, 480);
 	
+	private static final boolean DEBUG = false;
+	
 	/**
 	 * @param testTitle Title of the test window
 	 * @param toDisplay Container to display in the test window 
@@ -33,9 +40,75 @@ public class TestWindow extends JFrame {
         this.setMinimumSize(TestWindow.MINIMUM_SIZE);
         
         this.setTitle(testTitle);
-        
         this.setContentPane(toDisplay);
         this.pack();
+        
+        //TODO: Do copy this into our non-test window class when it comes around
+        
+        /* Centre the window on screen, this is hell if you have multiple
+         * monitors since the standard setLocationRelativeTo(null) won't work.
+         */
+        
+        //TODO: We could use a full-screen if we wanted to, just to show off
+        
+        GraphicsEnvironment graphicsEnvironment =
+        	GraphicsEnvironment.getLocalGraphicsEnvironment();
+        
+        // Check so that we actually have a graphical environment to render in
+        if (graphicsEnvironment.isHeadlessInstance()) {
+        	System.err.println("ERROR: No graphics environment found, this " +
+        			"application can not run in a command-line environment");
+        	System.exit(-1);
+        }
+        
+        GraphicsDevice graphicsDevice =
+        	graphicsEnvironment.getDefaultScreenDevice();
+        if (TestWindow.DEBUG) {
+        	System.err.println(this.getClass().getName() +
+        			": Will center window on device: "
+        			+ graphicsDevice.getIDstring());
+        }
+        
+        GraphicsConfiguration graphicsConfiguration =
+        	graphicsDevice.getDefaultConfiguration();
+        Rectangle bounds = graphicsConfiguration.getBounds();
+        if (TestWindow.DEBUG) {
+        	System.err.println(this.getClass().getName() +
+        			": Device default configuration bounds: " + bounds);
+        }
+        
+        // Verify that we can fit the MINIMUM window on screen
+        if (MINIMUM_SIZE.getWidth() > bounds.getWidth()
+        		|| MINIMUM_SIZE.getHeight() > bounds.getHeight()) {
+        	// At this stage we a graphical environment to display the error
+        	JOptionPane.showMessageDialog(null,
+        			"We require at least resolution of "
+        			+ (int) MINIMUM_SIZE.getWidth() + "x"
+        			+ (int) MINIMUM_SIZE.getHeight() + ", you are using "
+        			+ (int) bounds.getWidth() + "x"
+        			+ (int) bounds.getHeight(),
+        			"Error", JOptionPane.ERROR_MESSAGE);
+        	System.exit(-1);
+        }
+        
+        int xPos = (int) Math.max(0,
+        		(bounds.getWidth() - this.getWidth()) / 2);
+        int yPos = (int) Math.max(0,
+        		(bounds.getHeight() - this.getHeight()) / 2); 
+        
+        if (TestWindow.DEBUG) {
+        	System.err.println(this.getClass().getName()
+        			+ ": Will center on co-ordinates x: " + xPos
+        			+ " y: " + yPos);
+        }
+        
+        // Center us on this device
+        this.setLocation(xPos, yPos);
+
+        /*
+         * Make sure that this is ALWAYS after all calculations and
+         * configurations.
+         */
         this.setVisible(true);
 	}
 }

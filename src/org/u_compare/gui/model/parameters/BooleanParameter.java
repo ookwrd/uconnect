93361963db4fe89178aa230bab67da2036fde8c8
luke.mccrohon <luke.mccrohon@localhost>
2011-07-25 11:36:16 +0900

    Boolean and Integer Parameter moved over to new pattern.

--- src/org/u_compare/gui/model/parameters/BooleanParameter.java
+++ src/org/u_compare/gui/model/parameters/BooleanParameter.java
@@ -5,39 +5,24 @@ import org.u_compare.gui.model.parameters.constraints.ConstraintFailedException;
 public class BooleanParameter extends
 		AbstractParameter<Boolean> {
 
-	private Boolean parameter; //null represents unset.
-	
-	/**
-	 * 
-	 * @param name
-	 * @param description
-	 * @param value Null value represents value unset.
-	 */
-	public BooleanParameter(String name, String description, boolean mandatory, Boolean value) {
+	public BooleanParameter(String name, String description, boolean mandatory, Boolean parameter) {
 		super(name, description, mandatory, false);
-		this.parameter = value;
+		try {
+			add(parameter);
+		} catch (ConstraintFailedException e) {
+			System.out.println("Constraint failed on Parameter construction, this should not be possible.");
+			e.printStackTrace();
+		}
 	}
 	
 	public BooleanParameter(String name, String description, boolean mandatory, Boolean[] values) {
 		super(name, description, mandatory, true);
 		//this.parameter = value; TODO
 	}
-	
-	public Boolean getParameter(){
-		return parameter;
-	}
 
 	@Override
-	public void setValue(Boolean input) throws ConstraintFailedException {
-		
-		if(input != parameter){
-			parameter = input;
-			//No necessity to validate against constraints as can't constrain boolean parameters
-			notifyParameterValueChangedListeners();
-		}
-	}
-
 	public String getParameterString() {
+		Boolean parameter = getParameter();
 		if(parameter!=null){
 			return String.valueOf(parameter);
 		}else{

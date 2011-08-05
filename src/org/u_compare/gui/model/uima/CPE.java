f41b091bdcb0451256614656a58a4303e45a3dd5
luke.mccrohon <luke.mccrohon@localhost>
2011-08-05 18:54:53 +0900

    Storage based CPE class that passes read in read out tests.

--- src/org/u_compare/gui/model/uima/CPE.java
+++ src/org/u_compare/gui/model/uima/CPE.java
@@ -1,10 +1,11 @@
 package org.u_compare.gui.model.uima;
 
 import org.apache.uima.UIMAFramework;
+import org.apache.uima.collection.metadata.CpeCasProcessors;
 import org.apache.uima.collection.metadata.CpeCollectionReader;
+import org.apache.uima.collection.metadata.CpeConfiguration;
 import org.apache.uima.collection.metadata.CpeDescription;
 import org.apache.uima.collection.metadata.CpeDescriptorException;
-import org.apache.uima.resource.ResourceCreationSpecifier;
 import org.u_compare.gui.model.Workflow;
 
 public class CPE extends Workflow {
@@ -12,16 +13,29 @@ public class CPE extends Workflow {
 	//private ArrayList<CollectionReader> reader = new ArrayList<CollectionReader>();
 	
 	private CpeCollectionReader[] collectionReaders;
+	private CpeCasProcessors cpeCasProcessors;
+	private CpeConfiguration cpeConfiguration;
 	
 	public CPE(CpeDescription desc) throws CpeDescriptorException{
 		
 		collectionReaders = desc.getAllCollectionCollectionReaders();
-		
+		cpeCasProcessors = desc.getCpeCasProcessors();
+		cpeConfiguration = desc.getCpeConfiguration();
 		
 	}
 	
 	public CpeDescription getResourceCPEDescription(){
 		CpeDescription retVal = UIMAFramework.getResourceSpecifierFactory().createCpeDescription();
+		
+		try {
+			retVal.setAllCollectionCollectionReaders(collectionReaders);
+		} catch (CpeDescriptorException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		retVal.setCpeCasProcessors(cpeCasProcessors);
+		retVal.setCpeConfiguration(cpeConfiguration);
 		
 		return retVal;
 	}

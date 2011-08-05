7e0ba589a307667a9bcbf0807c66232249452a26
luke.mccrohon <luke.mccrohon@localhost>
2011-08-05 18:13:16 +0900

    CPE comparison test unit testing framework added.

--- src/org/u_compare/gui/model/uima/CPE.java
+++ src/org/u_compare/gui/model/uima/CPE.java
@@ -1,16 +1,29 @@
 package org.u_compare.gui.model.uima;
 
+import org.apache.uima.UIMAFramework;
+import org.apache.uima.collection.metadata.CpeCollectionReader;
 import org.apache.uima.collection.metadata.CpeDescription;
+import org.apache.uima.collection.metadata.CpeDescriptorException;
+import org.apache.uima.resource.ResourceCreationSpecifier;
 import org.u_compare.gui.model.Workflow;
 
 public class CPE extends Workflow {
 
-	private CollectionReader reader;
+	//private ArrayList<CollectionReader> reader = new ArrayList<CollectionReader>();
 	
-	public CPE(CpeDescription desc){
+	private CpeCollectionReader[] collectionReaders;
+	
+	public CPE(CpeDescription desc) throws CpeDescriptorException{
 		
-		//desc.ge
+		collectionReaders = desc.getAllCollectionCollectionReaders();
 		
+		
+	}
+	
+	public CpeDescription getResourceCPEDescription(){
+		CpeDescription retVal = UIMAFramework.getResourceSpecifierFactory().createCpeDescription();
+		
+		return retVal;
 	}
 	
 }

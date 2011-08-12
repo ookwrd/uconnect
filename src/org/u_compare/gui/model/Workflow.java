3baffc176312a7041ea1ed36eb23eae5f9cef9b3
luke.mccrohon <luke.mccrohon@localhost>
2011-08-12 20:49:16 +0900

    More detailed console messaging added to workflow.

--- src/org/u_compare/gui/model/Workflow.java
+++ src/org/u_compare/gui/model/Workflow.java
@@ -6,6 +6,9 @@ import java.util.TimerTask;
 import java.util.Timer;
 
 import org.apache.uima.UIMAFramework;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.collection.EntityProcessStatus;
+import org.apache.uima.collection.StatusCallbackListener;
 import org.apache.uima.collection.metadata.CpeDescription;
 import org.apache.uima.collection.metadata.CpeDescriptorException;
 import org.apache.uima.util.InvalidXMLException;
@@ -17,31 +20,31 @@ import org.u_compare.gui.model.uima.CPE;
  * Model class representing a UIMA Workflow. The workflow is modelled as a
  * special UIMAAggregateComponent.
  * 
- * @author luke mccrohon
+ * @author Luke McCrohon
  *
  */
-public class Workflow extends AbstractAggregateComponent {
-
-	//for debugging, delete when no longer needed.
-	private Timer timer = new Timer();
+public class Workflow extends AbstractAggregateComponent implements StatusCallbackListener {
 	
 	public enum WorkflowStatus {LOADING, READY, INITIALIZING, RUNNING, ERROR,
 		PAUSED, FINISHED};
 	
-	private WorkflowStatus status = WorkflowStatus.READY;
-	
-	private ArrayList<WorkflowStatusListener> workflowStatusListeners;
-	
 	public interface WorkflowStatusListener {
 		public void workflowStatusChanged(Workflow workflow);	
+	}	
+	
+	public interface WorkflowMessageListener{
+		public void workflowMessageSent(Workflow workflow, String message);
 	}
+	
+	private WorkflowStatus status = WorkflowStatus.READY;
+	private ArrayList<WorkflowStatusListener> workflowStatusListeners = new ArrayList<WorkflowStatusListener>();
+	private ArrayList<WorkflowMessageListener> workflowMessageListeners = new ArrayList<WorkflowMessageListener>();
 	
 	/**
 	 * Creates an empty workflow.
 	 */
 	public Workflow(){
-		super();		
-		workflowStatusListeners = new ArrayList<WorkflowStatusListener>();
+		super();
 	}
 	
 	
@@ -52,9 +55,7 @@ public class Workflow extends AbstractAggregateComponent {
 	 */
 	public Workflow(ArrayList<Component> components){
 		super();
-		
 		setSubComponents(components);
-		workflowStatusListeners = new ArrayList<WorkflowStatusListener>();
 	}
 	
 	/**
@@ -70,13 +71,15 @@ public class Workflow extends AbstractAggregateComponent {
 	 */
 	public void runWorkflow() throws InvalidStatusException {
 		
+notifyWorkflowMessageListeners("Play button hit.");
+		
 		if(status != WorkflowStatus.READY) {
 			throw new InvalidStatusException(status, WorkflowStatus.READY);
 		}
 		
 		setStatus(WorkflowStatus.INITIALIZING);
 		
-		//TODO
+		/*//TODO
 		
 		setStatus(WorkflowStatus.RUNNING);
 		
@@ -99,6 +102,8 @@ public class Workflow extends AbstractAggregateComponent {
 		};
 		
 		timer.schedule(task, 0, 1000);
+		
+		*/
 	}
 	
 	private void afterRunning(){
@@ -128,9 +133,13 @@ public class Workflow extends AbstractAggregateComponent {
 	 */
 	public void stopWorkflow() throws InvalidStatusException {
 		
+		notifyWorkflowMessageListeners("Stopped??!?!?");
+		
 		if(!(status == WorkflowStatus.RUNNING || status == WorkflowStatus.PAUSED)){
 			throw new InvalidStatusException(status, WorkflowStatus.RUNNING);
 		}
+		
+		notifyWorkflowMessageListeners("Stopped??!?!?");
 		
 		//TODO make this do something
 		
@@ -142,15 +151,10 @@ public class Workflow extends AbstractAggregateComponent {
 	 * @param newStatus
 	 */
 	private void setStatus(WorkflowStatus newStatus){
-		
-		if(status == newStatus){
-			return;
-		}
+		if(status == newStatus){return;}
 		
 		status = newStatus;
-		
 		notifyWorkflowStatusListeners();
-		
 	}
 	
 	/**
@@ -170,35 +174,28 @@ public class Workflow extends AbstractAggregateComponent {
 	 * @param listener
 	 */
 	public void registerWorkflowStatusListener(WorkflowStatusListener listener){
-		
-		workflowStatusListeners.add(listener);
-		
+		this.workflowStatusListeners.add(listener);
 	}
 	
 	/**
 	 * Should be called whenever the status of workflow processing is changed.
 	 */
-	private void notifyWorkflowStatusListeners(){
-		
+	protected void notifyWorkflowStatusListeners(){
 		for(WorkflowStatusListener listener : workflowStatusListeners){
 			listener.workflowStatusChanged(this);
 		}
-		
-		System.out.println("notify " + getStatus());
 	}
-
 	
-	public static void main(String[] args){
-		Workflow testUimaWorkflow = new Workflow();
-		
-		try {
-			testUimaWorkflow.runWorkflow();
-		} catch (InvalidStatusException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
+	public void registerWorkflowMessageListener(WorkflowMessageListener listener){
+		this.workflowMessageListeners.add(listener);
 	}
+	
+	protected void notifyWorkflowMessageListeners(String message){
+		for(WorkflowMessageListener listener : workflowMessageListeners){
+			listener.workflowMessageSent(this, message);
+		}
+	}
+	
 	
 	public static Workflow constructWorkflowFromXML(String location){
 		try {
@@ -218,7 +215,7 @@ public class Workflow extends AbstractAggregateComponent {
 			if(desc instanceof CpeDescription){
 				return new CPE((CpeDescription)desc);
 			} else {
-				//TODO error
+				//TODO error //TODO AS workflow
 				return null;
 			}
 			
@@ -231,6 +228,69 @@ public class Workflow extends AbstractAggregateComponent {
 			e.printStackTrace();
 			return null;
 		}
+	}
+
+
+	@Override
+	public void aborted() {
+		//TODO console message
+		
+		notifyWorkflowMessageListeners("Workflow processing aborted");
+		setStatus(WorkflowStatus.FINISHED);
+		
+		System.out.println("aborted");
+	}
+
+
+	@Override
+	public void batchProcessComplete() {
+		System.out.println("batchProcessComplete");
+		
+
+		setStatus(WorkflowStatus.FINISHED);
+		
+	}
+
+
+	@Override
+	public void collectionProcessComplete() {
+		// TODO Auto-generated method stub
+		System.out.println("CollectionProcessComplete");
+
+		setStatus(WorkflowStatus.FINISHED);
+	}
+
+
+	@Override
+	public void initializationComplete() {
+
+		setStatus(WorkflowStatus.RUNNING);
+	}
+
+
+	@Override
+	public void paused() {
+		// TODO Auto-generated method stub
+		System.out.println("paused");
+		
+
+		setStatus(WorkflowStatus.PAUSED);
+	}
+
+
+	@Override
+	public void resumed() {
+
+		System.out.println("resumed");
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void entityProcessComplete(CAS arg0, EntityProcessStatus arg1) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }

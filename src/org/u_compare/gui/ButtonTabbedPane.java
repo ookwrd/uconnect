5b372608c016e8c5cfc5574a5e4762104805b784
luke.mccrohon <luke.mccrohon@localhost>
2011-08-15 01:29:54 +0900

    ButtonTabbedPane now defines a JPanel to display if all other nonButton tabs have been removed.

--- src/org/u_compare/gui/ButtonTabbedPane.java
+++ src/org/u_compare/gui/ButtonTabbedPane.java
@@ -1,15 +1,24 @@
 package org.u_compare.gui;
 
+import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.dnd.DropTargetListener;
 import java.util.ArrayList;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 @SuppressWarnings("serial")
 public class ButtonTabbedPane extends JTabbedPane {
 
 	private final ArrayList<ButtonTabFlap> buttons = new ArrayList<ButtonTabFlap>();
+	
+	private JPanel panelZero = new JPanel(){
+		{
+			add(new JLabel("There are no Tabbed panes to display."));
+			setName("      ");
+		}
+	};
 	
 	public ButtonTabFlap addButtonTab(String title, DropTargetListener dropListener){
 		
@@ -18,7 +27,7 @@ public class ButtonTabbedPane extends JTabbedPane {
 		//Create a component that whenever it is displayed switches the tab to a previous one.
 		addTab(title, new JLabel(){
 			public void paint(Graphics g){//Makes the tab undisplayable, on attempts to draw it opens the last non-button
-				swtichToLastTab();
+				switchToLastTab();
 			}
 		});
 		
@@ -30,13 +39,34 @@ public class ButtonTabbedPane extends JTabbedPane {
 			
 	}
 	
+	@Override
+	public Component add(Component comp, int i){
+		
+		if(zeroInUse()){
+			remove(panelZero);
+			Component compent = super.add(comp, i);
+			return compent;
+		}else{
+			return super.add(comp, i);
+		}
+	}
+	
 	/**
 	 * Can't override getTabCount as it is used internally in JTabbedPane
 	 * 
 	 * @return
 	 */
 	public int numberOfNonButtonTabs(){
-		return super.getTabCount() - (buttons==null?0:buttons.size());//Somehow this seems to be called at construction time prior to initialization of buttons.
+		return super.getTabCount() - (buttons==null?0:buttons.size()) - (zeroInUse()?1:0);//Somehow this seems to be called at construction time prior to initialization of buttons.
+	}
+	
+	public boolean zeroInUse(){
+		for(Component comp :getComponents()){
+			if(comp.equals(panelZero)){
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	public int numberOfButtonTabs(){
@@ -46,9 +76,17 @@ public class ButtonTabbedPane extends JTabbedPane {
 	/**
 	 * Switch to the last non-button tab.
 	 */
-	public void swtichToLastTab(){
-		System.out.println(getTabCount() + "   " + buttons.size());
-		setSelectedIndex(numberOfNonButtonTabs()-1);
+	public void switchToLastTab(){
+		if(numberOfNonButtonTabs()>0){
+			setSelectedIndex(numberOfNonButtonTabs()-1);
+		}else{
+			add(panelZero, 0);
+			setSelectedIndex(0);
+		}
 	}
 	
+	public void setEmptyTab(JPanel panelZero){
+		this.panelZero = panelZero;
+		//TODO check if the old zero tab is in use and replace it
+	}
 }

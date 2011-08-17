7cd161d8a996c63d1501fdcca6cee3764de7eb21
luke.mccrohon <luke.mccrohon@localhost>
2011-08-17 17:58:34 +0900

    Cleaned up my Drop and Drag implementation a bit.

--- src/org/u_compare/gui/guiElements/LukesDragAndDropImplementation.java
+++ src/org/u_compare/gui/guiElements/LukesDragAndDropImplementation.java
@@ -1,6 +1,5 @@
 package org.u_compare.gui.guiElements;
 
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
@@ -19,30 +18,25 @@ import java.io.IOException;
 public class LukesDragAndDropImplementation {
 	
 	public static void registerDropTarget(final Component target, final DropController controller){
-		new DropTarget(target, new DropTargetAdapter(){
-			
+		new DropTarget(target, new DropTargetAdapter(){		
 			@Override
 			public void drop(DropTargetDropEvent arg0) {
-				System.out.println("Drop Lukes Target");
 				controller.somethingDroppedOnComponent();	
 			}
 			
 			@Override
 			public void dragExit(DropTargetEvent arg0) {
-				System.out.println("Drag exit Lukes target");
 				controller.setDragExit();
 			}
 			
 			@Override
 			public void dragEnter(DropTargetDragEvent arg0) {
-				controller.setDragEnter();
-				System.out.println("Drag enter Lukes target");				
+				controller.setDragEnter();			
 			}
 		});
 	}
 	
 	public interface DropController {
-		
 		/**
 		 * The current dragged component has been dropped on this component.
 		 */
@@ -63,29 +57,24 @@ public class LukesDragAndDropImplementation {
 		DragSource ds = DragSource.getDefaultDragSource();
 		ds.createDefaultDragGestureRecognizer(component, DnDConstants.ACTION_COPY,
 				new DragGestureListener(){
-			
 					@Override
 					public void dragGestureRecognized(DragGestureEvent dge) {
-						System.out.println("Lukes drag gesture recognized");
-						
 						controller.setDragged();
-						
-						dge.startDrag(DragSource.DefaultCopyDrop, new Transferable() {//TODO extract to fakeTransferable
-							protected /*static*/ DataFlavor componentFlavor = new DataFlavor(Color.class,
-							"A Color Object/wtf in DragSource??");//TODO is this needed?
-							@Override public boolean isDataFlavorSupported(DataFlavor flavor) {
-								return flavor.equals(componentFlavor);
-							}
-							@Override public DataFlavor[] getTransferDataFlavors() {
-								return new DataFlavor[]{ componentFlavor };
-							} 
-							@Override public Object getTransferData(DataFlavor flavor)
-									throws UnsupportedFlavorException, IOException {
-								return null;
-							}
-						});
+						dge.startDrag(DragSource.DefaultCopyDrop, new FakeTransferable());
 					}
 			
+					class FakeTransferable implements Transferable {
+						@Override public boolean isDataFlavorSupported(DataFlavor flavor) {
+							return false; 
+						}
+						@Override public DataFlavor[] getTransferDataFlavors() {
+							return new DataFlavor[]{ };
+						} 
+						@Override public Object getTransferData(DataFlavor flavor)
+								throws UnsupportedFlavorException, IOException {
+							return null;
+						}
+					}
 		});
 	}
 	

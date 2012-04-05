2c92ba1a7c5ce8d323356f3eafe2d484a2e7e465
luke.mccrohon <luke.mccrohon@localhost>
2012-04-05 23:06:29 +0900

    Comments.

--- src/org/u_compare/gui/component/ComponentTitleBar.java
+++ src/org/u_compare/gui/component/ComponentTitleBar.java
@@ -9,16 +9,23 @@ import org.u_compare.gui.control.ComponentController;
 import org.u_compare.gui.guiElements.RoundedBorder;
 import org.u_compare.gui.model.Component;
 
+/**
+ * The title bar of components/workflows. Appears as a white box with rounded
+ * black border for components, and just as the title for workflows.
+ * 
+ * @author Luke McCrohon
+ * 
+ */
 @SuppressWarnings("serial")
 public class ComponentTitleBar extends JPanel {
 
-	public ComponentTitleBar(ComponentController controller, Component component,
-			ComponentPanel innerPanel, boolean isWorkflow) {
+	public ComponentTitleBar(ComponentController controller,
+			Component component, ComponentPanel innerPanel, boolean isWorkflow) {
 
 		setLayout(new BorderLayout());
 		setOpaque(false);
 
-		if (!isWorkflow) {
+		if (!isWorkflow) {//Standard component
 			setBorder(new RoundedBorder(null, ComponentPanel.BORDER_COLOR,
 					ComponentPanel.HEADER_COLOR,
 					ComponentPanel.BORDER_ROUNDING,
@@ -27,19 +34,26 @@ public class ComponentTitleBar extends JPanel {
 			setupButtonPanel(this, innerPanel, controller, component);
 			setupTitlePanel(this, false, controller, component);
 		} else {//Workflow
+			//Workflows do not have a visible border
 			setBorder(new EmptyBorder(5, 5, 0, 0));
+			
+			//Also no button panel
 			setupTitlePanel(this, true, controller, component);
 		}
 
 	}
 
-	protected void setupTitlePanel(JPanel target, boolean isWorkflow, ComponentController controller, Component component) {
-		ComponentNamePanel titlePanel = new ComponentNamePanel(controller, component, isWorkflow, this);
+	protected void setupTitlePanel(JPanel target, boolean isWorkflow,
+			ComponentController controller, Component component) {
+		ComponentNamePanel titlePanel = new ComponentNamePanel(controller,
+				component, isWorkflow, this);
 		target.add(titlePanel, BorderLayout.CENTER);
 	}
 
-	protected void setupButtonPanel(JPanel target, ComponentPanel minimizable, ComponentController controller, Component component) {
-		TitleButtonPanel buttonPanel = new TitleButtonPanel(controller, component, minimizable);
+	protected void setupButtonPanel(JPanel target, ComponentPanel minimizable,
+			ComponentController controller, Component component) {
+		TitleButtonPanel buttonPanel = new TitleButtonPanel(controller,
+				component, minimizable);
 		target.add(buttonPanel, BorderLayout.EAST);
 	}
 
